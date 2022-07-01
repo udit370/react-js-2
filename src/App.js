eef6976e137e7ff1f0120cd2b237911ecacdaa79
Udit Kumar Singh <84468833+udit370@users.noreply.github.com>
2022-07-01 18:35:18 +0530

    Files Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { InterPage } from './Components/InterPage';
+
+function App() {
+  return (
+    <div className="App">
+      <InterPage/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/InterPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Style.css'
+import abc from './abc.png'
+
+export const InterPage = () => {
+  return (
+    <div className='box'>
+        <div className='table'>
+         <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+        <div className='table'>
+        <img className='pic' src={abc} alt="" />
+        </div>
+    </div>
+  )
+}
